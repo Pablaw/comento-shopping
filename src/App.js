e0efd2f517855e843156a973a079a3e1a23e512e
pablaw <>
2022-08-21 22:06:04 +0900

    feat: 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 import './App.css';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">프로젝트 시작</div>
+    <div className="App">
+      <Navigation />
+      프로젝트 시작</div>
   );
 }
 
