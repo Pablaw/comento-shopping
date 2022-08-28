4e63cef27c61da1e0f27b5674c40be68f43cb935
pablaw <>
2022-08-28 21:40:39 +0900

    feat: 라우팅설치, 페이지 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
+
 import './App.css';
-import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      코멘토 쇼핑
-      </div>
+    <BrowserRouter basename="comento-shopping">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productID" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
